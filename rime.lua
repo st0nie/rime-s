51c93af9daa8439d401c9088d267687f1e3378ad
ston <ston.jia@qq.com>
2023-06-16 13:51:29 +0800

    更新 lua
    Signed-off-by: ston <ston.jia@qq.com>

--- rime.lua
+++ rime.lua
@@ -1,3 +1,4 @@
 select_character_processor = require("select_character")
 date_translator = require("date_translator")
 reduce_english_filter = require("reduce_english_filter")
+autocap_filter = require("autocap_filter")
